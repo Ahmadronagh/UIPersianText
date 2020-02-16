44c3e6ea26472c59f493def0694b9bf462bc1965
Ahmad <a.ronagh@medrick.info>
2020-02-16 16:30:08 +0330

    Create UI Persian Text Utils

---
+++ Assets/Examples/ExamplePersianRichTextLoader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UIPersian;
+using UIPersian.Utils;
+using UnityEngine;
+
+public class ExamplePersianRichTextLoader : MonoBehaviour
+{
+    public RTLText RtlText;
+    
+    void Start()
+    {
+        TextAsset textAsset = Resources.Load<TextAsset>("data");
+        
+        var json = NiceJson.JsonObject.ParseJsonString(textAsset.text);
+        if (RtlText != null)
+        {
+            String persianText = json["value"].ToString();
+            RtlText.text = persianText;
+        }
+    }
+    
+}
---
+++ Assets/Scripts/Utils/UIPersianUtils.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace UIPersian.Utils
+{
+    public static class UIPersianUtils 
+    {
+        /// <summary>
+        /// convert text string for To unity display format
+        /// </summary>
+        /// <param name="text">string to fix</param>
+        /// <returns>Persian RTL string for display in Text Component</returns>
+        public static string ToPersian(this string text)
+        {
+            //ﺉﻚﻙﯤ
+            text = text.Replace('ی', 'ﻱ');
+            //str = str.Replace( 'ی','ﺉ');
+            text = text.Replace('ک', 'ﻙ');
+            //str = str.Replace('ﻚ', 'ک');
+            text = ArabicSupport.ArabicFixer.Fix(text,true,false);
+            text = text.Replace('ﺃ', 'آ');
+            return text;
+        }
+
+        /// <summary>
+        /// extention for convert text to unity persian format with rich text data Tag 
+        /// </summary>
+        /// <param name="text">string to fix</param>
+        /// <returns>Persian RTL string for display in Text Component</returns>
+
+        public static string ToPersianWithMeta(this string text)
+        {
+            if (String.IsNullOrEmpty(text))
+                return text;
+            string textWithMeta = "";
+            string[] output = Regex.Split(text, "(?=<)|(?<=>)");
+            for (int i = 0; i < output.Length; i++)
+            {
+                textWithMeta += output[i].Contains("<") ? output[i] : output[i].ToPersian();
+            }
+            return textWithMeta;
+        }
+
+        /// <summary>
+        /// extention for check if 
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static bool IsRTL(this string text)
+        {
+            var isRtl = false;
+            foreach (var _char in text)
+            {
+                if ((_char >= 1536 && _char <= 1791) || (_char >= 65136 && _char <= 65279))
+                {
+                    isRtl = true;
+                    break;
+                }
+            }
+            return isRtl;
+        }
+    }
+}
+
