ca618f0cccbf27e48656526da5cac487f4382109
Ahmad Ronagh <mr.a.ronagh@gmail.com>
2020-02-26 13:16:48 +0330

    Fix Tag Positioning Bug

--- Assets/Examples/ExamplePersianRichTextLoader.cs
+++ Assets/Examples/ExamplePersianRichTextLoader.cs
@@ -4,22 +4,28 @@ using System.Collections.Generic;
 using UIPersian;
 using UIPersian.Utils;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ExamplePersianRichTextLoader : MonoBehaviour
 {
     public RTLText RtlText;
+    public Text SampleText;
     
     void Start()
     {
         TextAsset textAsset = Resources.Load<TextAsset>("data");
         
         var json = NiceJson.JsonObject.ParseJsonString(textAsset.text);
-        if (RtlText != null)
-        {
-            String persianText = json["value"].ToString();
-            RtlText.text = persianText;
-        }
-        
+
+
+
+        SampleText.text = json["valuemetaa"].ToString().ToPersianWithMeta();
+//        if (RtlText != null)
+//        {
+//            String persianText = json["value"].ToString();
+//            RtlText.text = persianText;
+//        }
+
 //        Char NonBrakingSpace = (char)0x00a0;
 //        char x = (char)0x200d;
 //        textBox1.Text = "می@شود ".Replace('@', NonBrakingSpace);
--- Assets/Scripts/Utils/UIPersianUtils.cs
+++ Assets/Scripts/Utils/UIPersianUtils.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using UnityEngine;
 
 namespace UIPersian.Utils
 {
-    public static class UIPersianUtils 
+    public static class UIPersianUtils
     {
         /// <summary>
         /// convert text string for To unity display format
@@ -20,7 +21,7 @@ namespace UIPersian.Utils
             //text = text.Replace( 'ی','ﺉ');
             //text = text.Replace('ک', 'ﻙ');
             //text = text.Replace('ﻚ', 'ک');
-            text = ArabicSupport.ArabicFixer.Fix(text,true,false);
+            text = ArabicSupport.ArabicFixer.Fix(text, true, false);
             text = text.Replace('ﺃ', 'آ');
             return text;
         }
@@ -30,19 +31,86 @@ namespace UIPersian.Utils
         /// </summary>
         /// <param name="text">string to fix</param>
         /// <returns>Persian RTL string for display in Text Component</returns>
-
         public static string ToPersianWithMeta(this string text)
         {
             if (String.IsNullOrEmpty(text))
                 return text;
             string textWithMeta = "";
             string[] output = Regex.Split(text, "(?=<)|(?<=>)");
+
+
+            string onlyperisan = "";
             for (int i = 0; i < output.Length; i++)
             {
-                textWithMeta += output[i].Contains("<") ? output[i] : output[i].ToPersian();
+                if (!output[i].Contains("<"))
+                    onlyperisan += output[i];
             }
-            return textWithMeta;
+
+            onlyperisan = onlyperisan.ToPersian();
+            
+            int startIndex = 0;
+            int startTagCount = 0;
+            int endTagCount = 0;
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                if (output[i].Contains("</"))
+                {
+                    endTagCount += 1;
+                    int index = onlyperisan.Length - startIndex;
+                    var startTag = FindStartTag(output, endTagCount);
+                    onlyperisan = onlyperisan.Insert(index, startTag);
+                    startIndex += startTag.Length;
+                }
+                else if (output[i].Contains("<"))
+                {
+                    startTagCount += 1;
+                    int index = onlyperisan.Length - startIndex;
+                    var endTag = FindEndTag(output, startTagCount);
+                    onlyperisan = onlyperisan.Insert(index, endTag);
+                    startIndex += endTag.Length;
+                }
+                else
+                {
+                    startIndex += output[i].Length;
+                }
+            }
+
+            return onlyperisan;
         }
+
+        private static string FindStartTag(string[] output, int endTagCount)
+        {
+            int currentFindedEndTag = 0;
+            for (int j =  0; j < output.Length ; j++)
+            {
+                if (output[j].Contains("<") && !output[j].Contains("</"))
+                {
+                    currentFindedEndTag++;
+                    if (currentFindedEndTag == endTagCount)
+                        return output[j];
+                }
+            }
+
+            return "";
+        }
+
+        private static string FindEndTag(string[] output, int startTagCount)
+        {
+            int currentFindedEndTag = 0;
+            for (int j = output.Length - 1; j >= 0; j--)
+            {
+                if (output[j].Contains("</"))
+                {
+                    currentFindedEndTag++;
+                    if (currentFindedEndTag == startTagCount)
+                        return output[j];
+                }
+            }
+
+            return "";
+        }
+
 
         /// <summary>
         /// extention for check if 
@@ -60,8 +128,8 @@ namespace UIPersian.Utils
                     break;
                 }
             }
+
             return isRtl;
         }
     }
 }
-
